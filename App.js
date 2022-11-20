177c7af8f56093be19334968802cbd3a067ed40a
KYW <qodna25@gmail.com>
2022-11-20 19:23:45 +0900

    세 번째 커밋(영화앱)

--- App.js
+++ App.js
@@ -3,6 +3,13 @@ let mainContainer = document.querySelector('.main-container');
 let button = document.querySelectorAll('button');
 let search = document.getElementById('search');
 let title = document.getElementById('title');
+let cateBtn = document.getElementById('cate-btn');
+let category = document.querySelector('.category');
+let Body = document.querySelector('body');
+cateBtn?.addEventListener('click', () => {
+    let categoryState = category.style.display;
+    categoryState === 'none' ? category.style.display = 'flex' : category.style.display = 'none';
+});
 if (search instanceof HTMLElement)
     search.addEventListener('keydown', (e) => {
         if (search instanceof HTMLInputElement) {
@@ -32,21 +39,44 @@ button.forEach((btnEl) => {
 });
 let url;
 const Deduplication = async () => {
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    let result = data.results;
-    let contentHTML = '';
-    result.map((el, i) => {
-        return (contentHTML +=
-            `<div class="main-img">
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        let result = data.results;
+        let status = response.status;
+        let totalPages = data.total_pages;
+        let contentHTML = '';
+        result.map((el, i) => {
+            return (contentHTML +=
+                `<div class="main-img">
                 <div><img src="https://image.tmdb.org/t/p/w500${result[i].poster_path}"></img></div>
                 <div class="title" id="title">${result[i].title}</div>
                 <div class="title" id="title-date">(${result[i].release_date})</div>
             </div>`);
-    });
-    if (mainContainer instanceof Element)
-        mainContainer.innerHTML = contentHTML;
+        });
+        if (mainContainer instanceof Element) {
+            mainContainer.innerHTML = contentHTML;
+        }
+        ;
+        if (status == 200) {
+            if (totalPages == 0) {
+                throw new Error('검색된 자료가 존재하지 않습니다.');
+            }
+        }
+        else {
+            throw new Error(`${status}`);
+        }
+        ;
+    }
+    catch (error) {
+        alert(error);
+        if (mainContainer instanceof Element) {
+            mainContainer.innerHTML =
+                `<div class='error'>${error}</div>`;
+        }
+        ;
+    }
+    ;
 };
 //인기영화
 const popularMoive = () => {
--- App.ts
+++ App.ts
@@ -3,6 +3,17 @@ let mainContainer=document.querySelector('.main-container');
 let button=document.querySelectorAll('button');
 let search = document.getElementById('search');
 let title = document.getElementById('title');
+let cateBtn = document.getElementById('cate-btn');
+let category = document.querySelector('.category');
+let Body =document.querySelector('body');
+
+
+cateBtn?.addEventListener('click',()=>{
+   let categoryState = category.style.display;
+   categoryState==='none'? category.style.display='flex': category.style.display='none'
+    
+});
+
 
 
 if(search instanceof HTMLElement)
@@ -36,11 +47,12 @@ button.forEach((btnEl)=>{
 
 let url:URL;
 const Deduplication = async() =>{
-    
+    try{
     let response = await fetch(url);
     let data =await response.json();
-    console.log(data);
     let result = data.results
+    let status = response.status
+    let totalPages=data.total_pages
 
     let contentHTML =''
     result.map((el,i:number)=>{
@@ -54,18 +66,34 @@ const Deduplication = async() =>{
         );
     });
 
-    if(mainContainer instanceof Element)
-    mainContainer.innerHTML = contentHTML
+    if(mainContainer instanceof Element){ 
+        mainContainer.innerHTML = contentHTML};
+     
+    if(status==200){
+        if(totalPages == 0){
+            throw new Error('검색된 자료가 존재하지 않습니다.');
+        }
+     } else {
+        throw new Error(`${status}`);
+     };
     
+    } catch(error){
+        alert(error);
+        if(mainContainer instanceof Element){ 
+            mainContainer.innerHTML =
+        `<div class='error'>${error}</div>`
+        };
 
-}
+
+    };
+
+};
 
 
 
 //인기영화
 const popularMoive=()=>{
     if(title instanceof HTMLElement) title.innerHTML = '인기영화';
-
     url =new URL('https://api.themoviedb.org/3/movie/popular?api_key=dd5b2707a7e0bb5f2a03f34ba3f049bc&language=ko&page=1')
     Deduplication();
 };
